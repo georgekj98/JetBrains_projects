8a00b9bb4be2f0ddb7e776a64b7aff400468457f
George <georgekj1998@gmail.com>
2020-07-25 22:10:55 +0530

    Code works as intended. No bugs so far. Line spacing issues need to be sorted

--- Banking_system.py
+++ Banking_system.py
@@ -29,6 +29,7 @@ def db_insert(insert_num,insert_pin,insert_balance):
 def db_update(update_num, update_bal):
     update_query = f''' UPDATE card SET balance = {update_bal} WHERE number = {update_num}'''
     cursor.execute(update_query)
+    connection.commit()
 
 
 def db_retrieve(ret_number):
@@ -42,6 +43,13 @@ def db_retrieve(ret_number):
                         number =?;'''
     data = cursor.execute(ret_query,(ret_number,))
     return data.fetchone()
+
+
+def db_account_close(close_num):
+    del_query = f"DELETE FROM card WHERE number ={close_num}"
+    cursor.execute(del_query)
+    connection.commit()
+    print("The account has been closed!")
 
 
 def check_luhn(num):
@@ -67,6 +75,48 @@ class Account:
         self.ac_num = ac_num1
         self.pin = str(''.join(["{}".format(random.randint(0, 9)) for i in range(4)]))
         self.balance = 0
+
+
+def sub_menu(log_num):
+
+    while True:
+        log_num, log_pin, log_bal = db_retrieve(log_num)
+        choice_2 = int(input("\n1. Balance\n2. Add income\n3. Do transfer\n4. Close account\n5.Log out\n0. Exit\n"))
+        if choice_2 == 1:
+            print(f"Balance: {log_bal}\n")
+
+        elif choice_2 == 2:
+            deposit = int(input("\nEnter income:"))
+            db_update(log_num, int(log_bal+deposit))
+            print("Income was added!")
+
+        elif choice_2 == 3:
+            trans_num = input("\nTransfer\nEnter card number:")
+            if not (check_luhn(trans_num[:-1]) == trans_num[-1]):
+                print("\nProbably you made mistake in the card number. Please try again!")
+            else:
+                if not (db_check(trans_num)):
+                    print("\nSuch a card does not exist.")
+                else:
+                    trans_amt = int(input("\nEnter how much money you want to transfer:"))
+                    if trans_amt > log_bal:
+                        print("\nNot enough money!")
+                    else:
+                        trans_num, trans_pin, trans_bal = db_retrieve(trans_num)
+                        db_update(log_num, int(log_bal-trans_amt))
+                        db_update(trans_num, int(trans_bal+trans_amt))
+
+        elif choice_2 == 4:
+            db_account_close(log_num)
+            return 1
+
+        elif choice_2 == 5:
+            print("\nYou have successfully logged out!\n")
+            return 1
+
+        elif choice_2 == 0:
+            print("\nBye")
+            return 0
 
 
 # Establishing connection to the local database
@@ -107,18 +157,9 @@ while choice != 0:
             print("\nWrong card number or PIN!")
             continue
         print("\nYou have successfully logged in!")
+        choice = sub_menu(ac_num)
 
-        while True:
-            choice_2 = int(input("\n1. Balance\n2. Log out\n0. Exit\n"))
-            if choice_2 == 1:
-                print(f"Balance: {balance}\n")
-            elif choice_2 == 2:
-                print("\nYou have successfully logged out!\n")
-                break
-            elif choice_2 == 0:
-                choice = 0
-                print("\nBye")
-                break
+
 
 
 
