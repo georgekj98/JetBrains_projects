9ed08868ab758b45c58f10ef28de3e1951da80a3
George <georgekj1998@gmail.com>
2020-08-23 14:29:14 +0530

    New project : To_Do_List started

---
+++ To_Do_List.py
@@ -0,0 +1,10 @@
+# Write your code here
+def list_out():
+    print("Today:")
+    print("1) Do yoga")
+    print("2) Make breakfast")
+    print("3) Learn basics of SQL")
+    print("4) Learn what is ORM")
+
+
+list_out()
