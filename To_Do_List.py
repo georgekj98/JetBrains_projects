ee9ff24e3d1368611deb5386f1e8a2726e1d2809
George <georgekj1998@gmail.com>
2020-09-12 17:00:09 -0500

    Functionalities for checkpoint 2 all working spacing issues yet to be resolved

--- To_Do_List.py
+++ To_Do_List.py
@@ -16,14 +16,58 @@ class Task(Base):
     deadline = Column('deadline', Date, default=datetime.today())
 
 
-def main():
+def print_tasks():
 
-    engine = create_engine('sqlite:////todo.db?check_same_thread=False', echo=True)
-    Base.metadata.create_all(engine)
+    print("\nToday:")
+    rows = session.query(Task).all()
+    if len(rows) == 0:
+        print("Nothing to do!")
+    for i, row in enumerate(rows):
+        print(f"{str(i+1)}. {row.task}")
+    print("\n")
+    return
+
+
+def add_task(new_task):
+    rows = session.query(Task).all()
+    new_id = len(rows) + 1
+    task = Task()
+    task.id = new_id
+    task.task = new_task
+    session.add(task)
+    session.commit()
+    return
+
+
+def cli():
+
+    choice = 123
+    while choice:
+        choice = input("1) Today's tasks\n2) Add task\n0) Exit\n")
+        if choice == '0':
+            print("\nBye!")
+            return
+        elif choice == '1':
+            print_tasks()
+        elif choice == '2':
+            new_task = input("\nEnter task")
+            add_task(new_task)
+            print("The task has been added!")
 
 
 if __name__ == '__main__':
-    main()
+    engine = create_engine('sqlite:////todo.db?check_same_thread=False')
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    cli()
+    #task_1 = Task()
+    #task_1.id = 2
+    #task_1.task = "Okay just making sure"
+    #session.add(task_1)
+    #session.commit()
+    session.close()
+
 
 ''' def list_out():
         print("Today:")
