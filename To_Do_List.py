476b6c1371b01281dad30a2b38549f64e9466646
George <georgekj1998@gmail.com>
2020-09-12 16:02:10 -0500

    table schema defined. database is created

--- To_Do_List.py
+++ To_Do_List.py
@@ -1,10 +1,37 @@
-# Write your code here
-def list_out():
-    print("Today:")
-    print("1) Do yoga")
-    print("2) Make breakfast")
-    print("3) Learn basics of SQL")
-    print("4) Learn what is ORM")
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, Integer, String, Date
+from datetime import datetime
+from sqlalchemy.orm import sessionmaker
 
 
-list_out()
+Base = declarative_base()
+
+
+class Task(Base):
+
+    __tablename__ = "task"
+    id = Column('id', Integer, primary_key=True)
+    task = Column('task', String)
+    deadline = Column('deadline', Date, default=datetime.today())
+
+
+def main():
+
+    engine = create_engine('sqlite:////todo.db?check_same_thread=False', echo=True)
+    Base.metadata.create_all(engine)
+
+
+if __name__ == '__main__':
+    main()
+
+''' def list_out():
+        print("Today:")
+        print("1) Do yoga")
+        print("2) Make breakfast")
+        print("3) Learn basics of SQL")
+        print("4) Learn what is ORM")
+
+
+list_out()'''
+
