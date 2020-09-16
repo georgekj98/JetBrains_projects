7085a52e7d6414c08b20d2264a93a58a8f9bbb9a
George <georgekj1998@gmail.com>
2020-09-16 16:55:49 -0500

    converting to OOPS format

--- To do list/To_Do_List.py
+++ To do list/To_Do_List.py
@@ -5,65 +5,69 @@ from datetime import datetime
 from sqlalchemy.orm import sessionmaker
 
 
-Base = declarative_base()
+class To_Do_List():
+    Base = declarative_base()
 
+    class Task(Base):
 
-class Task(Base):
+        __tablename__ = "task"
+        id = Column('id', Integer, primary_key=True)
+        task = Column('task', String)
+        deadline = Column('deadline', Date, default=datetime.today())
 
-    __tablename__ = "task"
-    id = Column('id', Integer, primary_key=True)
-    task = Column('task', String)
-    deadline = Column('deadline', Date, default=datetime.today())
+    def __init__(self, db_name):
 
+        engine = create_engine(f'sqlite:///{db_name}?check_same_thread=False')
+        self.Base.metadata.create_all(engine)
+        Session = sessionmaker(bind=engine)
+        self.session = Session()
 
-def print_tasks():
+    def print_tasks(self):
 
-    print("\nToday:")
-    rows = session.query(Task).all()
-    if len(rows) == 0:
-        print("Nothing to do!")
-    for i, row in enumerate(rows):
-        print(f"{str(i+1)}. {row.task}")
-    print()
-    return
+        print("\nToday:")
+        rows = self.session.query(self.Task).all()
+        if len(rows) == 0:
+            print("Nothing to do!")
+        for i, row in enumerate(rows):
+            print(f"{str(i+1)}. {row.task}")
+        print()
+        return
 
+    def add_task(self,new_task):
+        rows = self.session.query(self.Task).all()
+        new_id = len(rows) + 1
+        task = self.Task()
+        task.id = new_id
+        task.task = new_task
+        self.session.add(task)
+        self.session.commit()
+        return
 
-def add_task(new_task):
-    rows = session.query(Task).all()
-    new_id = len(rows) + 1
-    task = Task()
-    task.id = new_id
-    task.task = new_task
-    session.add(task)
-    session.commit()
-    return
+    def cli(self):
 
-
-def cli():
-
-    choice = 123
-    while choice:
-        choice = input("1) Today's tasks\n2) Add task\n0) Exit\n")
-        if choice == '0':
-            print("\nBye!")
-            return
-        elif choice == '1':
-            print_tasks()
-        elif choice == '2':
-            new_task = input("\nEnter task\n")
-            add_task(new_task)
-            print("The task has been added!\n")
+        choice = 123
+        while choice:
+            choice = input("1) Today's tasks\n2) Add task\n0) Exit\n")
+            if choice == '0':
+                print("\nBye!")
+                return
+            elif choice == '1':
+                self.print_tasks()
+            elif choice == '2':
+                new_task = input("\nEnter task\n")
+                self.add_task(new_task)
+                print("The task has been added!\n")
 
 
 if __name__ == '__main__':
-    engine = create_engine('sqlite:///todo_list.db?check_same_thread=False')
+   ''' engine = create_engine('sqlite:///todo_list.db?check_same_thread=False')
     Base.metadata.create_all(engine)
     # Task.__table__.create(engine)
     Session = sessionmaker(bind=engine)
     session = Session()
     cli()
     #Base.metadata.drop_all(bind=engine)
-    session.close()
+    session.close()'''
 
 
 ''' def list_out():
